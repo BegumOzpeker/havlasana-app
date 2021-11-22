08aa9e8b90af2b79b51279169dffc8d55dec6763
begumozpeker <b.ozpeker@hotmail.com>
2021-11-22 18:19:53 +0300

    the core done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+    @members = [ 'begum', 'ege' ]
+    if params[:member]
+    @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root                   to: 'pages#home'
+  get 'about',           to: 'pages#about',      as: :about
+  get 'contact',         to: 'pages#contact',    as: :contact
+
 end
