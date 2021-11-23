a6fdd79349c6dcbc00a60d14c53b36797336e0bd
begumozpeker <b.ozpeker@hotmail.com>
2021-11-23 16:34:46 +0300

    dogcontroller done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,38 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+    @markers = @dogs.geocoded.map do |chef|
+      {
+        lat: chef.latitude,
+        lng: chef.longitude
+      }
+    end
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+
+
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chef_params
+    params.require(:dog).permit(:dog_name, :location, :breed, :description)
+  end
 end
