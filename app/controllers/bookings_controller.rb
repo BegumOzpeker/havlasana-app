69c67e0325a94aa53835caf6b27e4ed495baebb9
begumozpeker <b.ozpeker@hotmail.com>
2021-11-23 17:37:04 +0300

    controllers fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,14 +11,9 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     if @booking.end_time < @booking.start_time
     else
-<<<<<<< HEAD
       @booking.save
     end
 
-=======
-    @booking.save
-    end
->>>>>>> e9af6c79323f2688ea2807387380ee2711add2cb
   redirect_to dashboard_path
   end
 
@@ -33,8 +28,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_time, :end_time, :notes)
   end
-<<<<<<< HEAD
-
-=======
->>>>>>> e9af6c79323f2688ea2807387380ee2711add2cb
 end
