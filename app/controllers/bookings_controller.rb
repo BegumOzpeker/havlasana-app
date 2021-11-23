c5cb5efb17d9c73aef011b2c22b5e4126f4127b7
Ege Cakmak <egecakmak@hotmail.com>
2021-11-23 17:18:06 +0300

    syntax errors fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+  def new
+    @dog = Dog.find(params[:dog_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @dog = Dog.find(params[:dog_id])
+    booking.dog = @dog
+    @booking.user = current_user
+    if @booking.end_time < @booking.start_time
+    else
+      @booking.save
+    end
+
+  redirect_to dashboard_path
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :notes)
+  end
+
 end
