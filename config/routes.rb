0a21ca3e185d8f60fe31c96d8ff264dc0acdc72e
Ege Cakmak <egecakmak@hotmail.com>
2021-11-26 18:13:01 +0300

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root                   to: 'pages#home'
   get 'about',           to: 'pages#about',      as: :about
   get 'contact',         to: 'pages#contact',    as: :contact
+
+  resources :dogs, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookings, only: [:create, :new]
+  end
+
+  resources :bookings, only: [:destroy]
+  resources :dashboard, only: [:show]
 
 end
